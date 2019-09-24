0f05bc9aee4f57e16537d4a1f1b4ec67d4c9214c
abedin it ltd <abedinit.itltd@gmail.com>
2019-09-24 19:44:59 +0600

    commit

---
+++ admin/admin.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css">
+<link rel="stylesheet" href="style.css">
+<link rel="shortcut icon" href="img/logo.png" />
+<title>Abedin It Limited</title>
+</head>
+<body>
+
+<div class="container">
+    <div class="row">
+
+  
+    <div class="col-md-8 login-form-1 offset-2 card mt-5">
+    <center style="margin-top: -28px;">
+    <div class="div_class">
+        <img src="./img/logo.png" alt="">
+
+    </div>
+    </center>
+                    <h3 class="text-center mb-3">Login </h3>
+                    <form>
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Email *" name="email" />
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" placeholder=" Password *" name="password" />
+                        </div>
+                        <div class="form-group text-center">
+                            <a href="#" name="submit" class="btnSubmit btn btn-primary" value="Login">Login </a>
+                        </div>
+                        
+                    </form>
+                </div>
+    </div>
+</div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.15.0/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ medicine_table/index.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>Medicine</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="./style.css">
+<link rel="shortcut icon" href="admin/img/logo.png" />
+
+
+<style type="text/css">
+    .bs-example{
+    	margin: 20px;
+    }
+</style>
+</head>
+<body>
+<div class="bs-example" style="border:1px solid ">
+    <table  >
+    <h3>Medicin Data entry</h3>
+    <form action="">
+
+     
+        <tbody>
+            <tr>
+                <td>Company Name: </td>
+                 <td> <input type="text" value="abedinitltd">
+                </td>
+                <td>Village</td>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td>Form No</td>
+                <td><input type="text"></td>
+                <td>Region</td>
+                <td> <input type="text"></td>
+            </tr>
+         
+            
+            <tr>
+                <td>Colle Date:
+                    <input type="date" name="date">
+
+                </td>
+                <td>Press:
+                    <input type="date" name="date">
+                </td>
+                <td>Area</td>
+                <td><input type="text"></td>
+            </tr> 
+            <tr>
+                <td>Route:</td>
+                <td><input type="text"></td>
+                <td>Teritory</td>
+                <td> <input type="text"></td>
+            </tr>
+            <tr>
+                <td>Distric:</td>
+                <td>
+                <select name="distric">
+                    <option disabled >Chose District</option>
+                    <option >Dhaka</option>
+                    <option >Cumilla</option>
+                    <option >Foridpur</option>
+                    <option >Feni</option>
+                    </select>
+                </td></td>
+                <td>Market</td>
+                <td> <input type="text"></td>
+            </tr>
+            <tr>
+                <td>Thana:</td>
+                <td>
+                <select name="thana">
+                    <option  >Chose Thana</option>
+                    <option >Dhaka</option>
+                    <option >Cumilla</option>
+                    <option >Foridpur</option>
+                    <option >Feni</option>
+                    </select>
+                <td>Inst/institute</td>
+                <td> <input type="text"></td>
+            </tr>
+
+            <tr>
+                <td>Union:</td>
+                <td>
+                <select name="union">
+                    <option  >Chose union</option>
+                    <option >Dhaka</option>
+                    <option >Cumilla</option>
+                    <option >Foridpur</option>
+                    <option >Feni</option>
+                    </select>
+                <td>Inst/institute</td>
+                <td> <input type="text"></td>
+            </tr>
+            <tr>
+                <td>Doctor Info :</td>
+                <td>Serial No:</td>
+                <td>Type</td>
+                <td> <input type="text"></td>
+            </tr>
+            <tr>
+                <td><input type="text"></td>
+                <td><input type="text"></td>
+                <td><input type="text"></td>
+                <td> <input type="text"></td>
+            </tr>
+    <button type="submit" class="ml-5 btn btn-primary"> submit</button>
+         
+        </tbody> 
+    </table>
+    <button type="submit" class="ml-5 btn btn-primary"> submit</button>
+
+</div>
+</form>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>        
